2c92eddec2ed8f48271ac93535686fb1cd48e168
PranoyAcharyya <acharyapranoy@gmail.com>
2026-04-15 15:38:19 +0530

    push

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,11 +1,24 @@
-
-import { fetchProducts } from "@/lib/productService";
-
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const category = searchParams.get("category") || "all";
+  const category = searchParams.get("category") || "";
 
-  const data = await fetchProducts(category);
+  const apiUrl =
+    category && category !== "all"
+      ? `https://fakestoreapi.com/products/category/${category}`
+      : `https://fakestoreapi.com/products`;
+
+  // USE PROXY (IMPORTANT)
+  const proxyUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(apiUrl)}`;
+
+  const res = await fetch(proxyUrl);
+
+  if (!res.ok) {
+    console.error("API FAILED");
+    return Response.json([], { status: 200 });
+  }
+
+  const json = await res.json();
+  const data = JSON.parse(json.contents);
 
   return Response.json(data);
 }
